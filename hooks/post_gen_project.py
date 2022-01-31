8446f6c977bbfff089d8c491248720667cb4ebcb
abitha-thankaraj <18137006+abitha-thankaraj@users.noreply.github.com>
2022-01-30 20:24:24 -0500

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -25,10 +25,10 @@ _____':::::_____________________________________\__\_________________________
 If you have not done so already, create a conda environment for your new 
 project with:
 
-cd {{cookiecutter.repo_name}}/setup
+cd {{cookiecutter.repo_name}}
 conda create --name {{cookiecutter.repo_name}}
 conda activate {{cookiecutter.repo_name}}
-conda env export > environment.yml
+conda env export > setup/environment.yml
 
 Install your new project in your local conda environment with:
 
